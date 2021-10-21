fb975b0f9091e5e10b78065e818d9afe9c6eb980
faithelizagreen <faithgreen@knights.ucf.edu>
2021-10-21 14:22:52 -0400

    set up server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
