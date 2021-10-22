c1f1fdc5ccd5b103fec55f661223df3efd29c897
faithelizagreen <faithgreen@knights.ucf.edu>
2021-10-22 19:07:14 -0400

    added delete button

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const notes = require('express').Router()
-const {readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const {readFromFile, readAndAppend, deleteNote} = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all the notes
@@ -7,7 +7,7 @@ notes.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
+// POST Route for a new note
 notes.post('/api/notes', (req, res) => {
   console.log(req.body);
 
@@ -19,11 +19,19 @@ notes.post('/api/notes', (req, res) => {
       text,
       note_id: uuid(),
     };
+    // Read JSON and append new note
     readAndAppend(newNote, './db/db.json');
     res.json(`Note added successfully 🚀`);
   } else {
     res.json('Error in adding note');
   }
 });
+
+// Delete note
+notes.delete('/notes:id', (req, res) => {
+  console.log(req.params.id);
+  deleteNote(req.params.id, './db/db.json');
+  res.json('success')
+})
 
 module.exports = notes;
