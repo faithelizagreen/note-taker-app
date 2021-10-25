6650f687f7041db455db8f932722a6b3811acd13
faithelizagreen <faithgreen@knights.ucf.edu>
2021-10-24 22:44:36 -0400

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const api = require('./routes/api-routes')
 const html= require('./routes/html-routes')
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
